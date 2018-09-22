42fff0a286782393c00a4bfeefe7ce0f92a1fcdb
Peppercornies <bas43@cam.ac.uk>
2018-09-22 12:59:46 +0100

    Mad Libs

---
+++ mad_libs.py
@@ -0,0 +1,8 @@
+
+noun = ' ' + input("Give a Noun:")
+verb = ' ' + input('Give a Verb in Past Tense:')
+adjective = ' ' + input('Give a Adjective:')
+noun2 = ' ' +input('Give another Noun:')
+
+sentance = 'The' + noun + verb + adjective + ' with the' + noun2
+print(sentance)
